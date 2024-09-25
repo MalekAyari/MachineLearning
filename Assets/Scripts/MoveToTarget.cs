226e8f3ebec328337f80417239d6e970e4e7d7ba
Malek Ayari <56804495+MalekAyari@users.noreply.github.com>
2024-09-25 15:27:23 +0100

    test init

--- Assets/Scripts/MoveToTarget.cs
+++ Assets/Scripts/MoveToTarget.cs
@@ -14,7 +14,7 @@ public class MoveToTarget : Agent
     public float moveZ;
     [SerializeField] private List<Transform> checkpoints;
     private Transform nextCheckpoint;
-    private int checkpoint;
+    private int currentCheck = 0;
     private float distanceToCheckpoint;
     private float yLevel;
     [SerializeField] private int ObstaclesPerFloor;
@@ -22,44 +22,45 @@ public class MoveToTarget : Agent
     [SerializeField] private List<Transform> obstaclePositions1 = new List<Transform>();
     [SerializeField] private List<Transform> obstaclePositions2 = new List<Transform>();
     [SerializeField] private List<Transform> obstaclePositions3 = new List<Transform>();
-
-    public void RandomizePosition(List<Transform> list, int floor){
-        
+    [SerializeField] private List<Transform> stairs = new List<Transform>();
+    public void RandomizePosition(List<Transform> list, int floor)
+    {
         List<int> indexes = new List<int>();
-        for (int i = 0; i < ObstaclesPerFloor; i++){
+        for (int i = 0; i < ObstaclesPerFloor; i++)
+        {
             int index;
-
-            do {
+            do
+            {
                 index = Random.Range(0, list.Count);
             } while (indexes.Contains(index));
 
             indexes.Add(index);
-
-            obstacles[i + ObstaclesPerFloor*(floor-1)].position = list[index].position;
+            obstacles[i + ObstaclesPerFloor * (floor - 1)].position = list[index].position;
         }
     }
 
     public override void OnEpisodeBegin()
     {
         characterAnimationManager.OnResetEpisode();
-
         StartCoroutine(Wait(0.5f));
 
         yLevel = transform.localPosition.y;
 
-        foreach (Transform obj in checkpoints){
+        foreach (Transform obj in checkpoints)
+        {
             obj.gameObject.SetActive(true);
         }
 
-        nextCheckpoint = checkpoints[0];
-        checkpoint = 0;
+        currentCheck = 0;
+        nextCheckpoint = checkpoints[currentCheck];
 
-        RandomizePosition(obstaclePositions1, 1);
-        RandomizePosition(obstaclePositions2, 2);
-        RandomizePosition(obstaclePositions3, 3);
+        //RandomizePosition(obstaclePositions1, 1);
+        //RandomizePosition(obstaclePositions2, 2);
+        //RandomizePosition(obstaclePositions3, 3);
     }
 
-    IEnumerator Wait(float delay){
+    IEnumerator Wait(float delay)
+    {
         yield return new WaitForSeconds(delay);
     }
 
@@ -68,14 +69,18 @@ public class MoveToTarget : Agent
         sensor.AddObservation(transform.localPosition);
         sensor.AddObservation(distanceToCheckpoint);
 
-        // sensor.AddObservation(nextCheckpoint.localPosition - transform.localPosition);
-        
-        //Recheck logic
-        foreach (Transform checkpoint in checkpoints){
+        foreach (Transform checkpoint in checkpoints)
+        {
             sensor.AddObservation(checkpoint.localPosition);
         }
+        
+        foreach (Transform stair in stairs)
+        {
+            sensor.AddObservation(stair.localPosition);
+        }
 
-        foreach (Transform obstacle in obstacles){
+        foreach (Transform obstacle in obstacles)
+        {
             sensor.AddObservation(obstacle.transform.localPosition);
         }
     }
@@ -84,16 +89,25 @@ public class MoveToTarget : Agent
     {
         moveX = actions.ContinuousActions[0];
         moveZ = actions.ContinuousActions[1];
-        
-        AddReward(-0.0001f);
 
-        //Adds reward the closer they get to checkpoint
-        if (distanceToCheckpoint > Vector3.Distance(transform.localPosition, nextCheckpoint.localPosition)){
-            AddReward(0.01f);
+        AddReward(-0.00001f);
+
+        // Check if the agent has gotten closer to the checkpoint
+        float currentDistance = Vector3.Distance(transform.localPosition, nextCheckpoint.localPosition);
+        if (distanceToCheckpoint > currentDistance)
+        {
+            AddReward(0.00002f);
+        } else
+        {
+            AddReward(-0.00001f);
         }
-        distanceToCheckpoint = Vector3.Distance(transform.localPosition, nextCheckpoint.localPosition);
+        distanceToCheckpoint = currentDistance;
 
-        if (yLevel > transform.localPosition.y) AddReward(-0.5f); 
+        // Reward for moving upward
+        if (yLevel < transform.localPosition.y)
+        {
+            AddReward(0.5f);
+        }
         yLevel = transform.localPosition.y;
     }
 
@@ -109,19 +123,23 @@ public class MoveToTarget : Agent
         if (other.gameObject.CompareTag("Reward"))
         {
             other.gameObject.SetActive(false);
-            AddReward(50f);
-            nextCheckpoint = checkpoints[++checkpoint];
-            Debug.Log("Checkpoint reached!");
+            AddReward(20); 
+            currentCheck++;
+
+            if (currentCheck < checkpoints.Count)
+            {
+                nextCheckpoint = checkpoints[currentCheck];
+                Debug.Log($"Checkpoint {currentCheck} reached!");
+            }
+            else
+            {
+                Debug.Log("All checkpoints reached!");
+                Time.timeScale = 0f;
+            }
         }
-        if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("obstacle"))
+        else if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("obstacle"))
         {
-            AddReward(-10);
-            characterAnimationManager.characterController.enabled = false;
-            EndEpisode();
-        }
-        if (other.gameObject.CompareTag("Wall2"))
-        {
-            AddReward(-20);
+            AddReward(-5);
             characterAnimationManager.characterController.enabled = false;
             EndEpisode();
         }
